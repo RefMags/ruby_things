c27f054d7f8f6e7478de33d97fd20a8cbec48bd2
refilwe magwaneng <fyfymags@gmail.com>
2023-07-26 22:01:54 +0900

    methods exercises added~ongoing

---
+++ RB100/mutate.rb
@@ -0,0 +1,38 @@
+#Mutating Arguments
+a = [1, 2, 3]
+
+#Example of a method definition that mutates its arguments permanently
+def mutate(array)
+  array.pop
+end
+
+p "Before mutate method : #{a}"
+mutate(a)
+p "After mutate method: #{a}"
+
+# the array is permanently modified that the local variable 'a' references.
+#How? by passing it to the mutate method even though it is outside the method's definition scope.
+#Why? the 'pop' method mutates its calling object(the array specified by array)
+#a is still pointing to the same array, but the number of elements changes
+
+#Contrast with another code
+a = [1, 2, 3]
+#Example of a method definition that does not mutate the argument
+def no_mutate(array)
+  array.last #last method does not mutate the calling object 'array' hence the array passed to 'no_mutate' is not mutated
+end
+
+p "Before no_mutate method: #{a}"
+no_mutate(a)
+p "After no_mutate method: #{a}"
+
+
+#puts vs return: The Sequel
+a = [1, 2, 3]
+
+def mutate(array)
+  array.pop
+end
+p "Before mutate method: #{a}"
+mutate(a)
+p ""
---
+++ RB100/say.rb
@@ -0,0 +1,48 @@
+puts "hello"
+puts "hi"
+puts "how are you"
+puts "I am fine"
+
+def say(words)
+  puts words + '.' # Ability to make one change here
+end
+
+say("hello") #hello is passed on as argument in place for the words parameter. Then the code within the method definition is executed with the words local variable evaluated to "hello"
+say("hi")
+say("how are you")
+say("I am fine")
+
+#Method definition and local variable scope
+
+a = 5
+
+def some_method
+  a = 3
+end
+
+puts a #output 5 because method definitions create their own scope that's entirely outside of the execution flow.
+
+#method invocation with a block
+[1, 2, 3].each do |num|
+  puts num
+end
+
+# method definition
+def print_num(num)
+  puts num
+end
+
+
+#Mutating Argument
+
+def some_method(number) #2. in the method, the value of a is assigned a local variable number which is scoped at the method definition level.
+  number = 7 #number is reassigned the value 7
+end
+
+a = 5
+some_method(a) #1. a is passed in to the method
+puts a
+
+#Does the reassigment of the number to 1 affect a's output?
+# No, since number is scoped at the method definition level and a's value remains unchanged
+# Method definitions cannot modify arguments passed into them permanently.
