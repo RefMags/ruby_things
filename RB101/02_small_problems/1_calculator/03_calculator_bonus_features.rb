fa9938b4bea96ebca4fe7b984ba93002071003f4
refilwe magwaneng <fyfymags@gmail.com>
2023-10-09 09:57:33 +0900

    modification to bonus feature comments

--- RB101/02_small_problems/1_calculator/03_calculator_bonus_features.rb
+++ RB101/02_small_problems/1_calculator/03_calculator_bonus_features.rb
@@ -13,7 +13,8 @@
 #   How can we build a validating method, called number? to verify that only
 #   numbers --integers or floats-- are entered?
 # 3. Derisk the "operation_to_message" method to accomodate for additional code
-# that may be added after the "case" statement within the method. Add some changes
+  # that may be added after the "case" statement within the method.
+  # Add some changes
 # 4. Extracting messages in the program to a configuration file.
 #   Because there are lots of messages to the program, we could move them into a
 #   configuration file and access by key.
