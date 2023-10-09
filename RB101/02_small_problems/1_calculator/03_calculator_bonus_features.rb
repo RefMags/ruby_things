a60e29d93035524a416000cc959ba815fe5ca5c0
refilwe magwaneng <fyfymags@gmail.com>
2023-10-09 09:55:30 +0900

    modified files

--- RB101/02_small_problems/1_calculator/03_calculator_bonus_features.rb
+++ RB101/02_small_problems/1_calculator/03_calculator_bonus_features.rb
@@ -7,15 +7,17 @@
 # REFACTORING CODE #
 
 # ADDING BONUS FEATURES #
- 1. Better integer validation
-  currently its not fully accurate, as 0 cannot be entered.
-2. Number validation
-  How can we build a validating method, called number? to verify that only
-  numbers --integers or floats-- are entered?
-3. Derisk the "operation_to_message" method to accomodate for additional code
-that may be added after the "case" statement within the method. Add some changes
-4. Extracting messages in the program to a configuration file.
-  Because there are lots of messages to the program, we could move them into a
-  configuration file and access by key.
-5. Internationalize the message in your calculator.
+# 1. Better integer validation
+#   currently its not fully accurate, as 0 cannot be entered.
+# 2. Number validation
+#   How can we build a validating method, called number? to verify that only
+#   numbers --integers or floats-- are entered?
+# 3. Derisk the "operation_to_message" method to accomodate for additional code
+# that may be added after the "case" statement within the method. Add some changes
+# 4. Extracting messages in the program to a configuration file.
+#   Because there are lots of messages to the program, we could move them into a
+#   configuration file and access by key.
+# 5. Internationalize the message in your calculator.
   Using the configuration file,send message to call the right translation to cod
+  validate that only a language is choosen/ message that only certain language
+  exists
