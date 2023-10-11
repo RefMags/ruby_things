141b9f1a51d7a8d8e192e5a37205873c84a18496
refilwe magwaneng <fyfymags@gmail.com>
2023-10-12 07:04:33 +0900

    completed calculator refactoring

--- RB101/02_small_problems/1_calculator/03_calculator_bonus_features.rb
+++ RB101/02_small_problems/1_calculator/03_calculator_bonus_features.rb
@@ -5,6 +5,7 @@
 # Perform an operation of the two numbers and display results.
 
 # REFACTORING CODE #
+# suppose we want to change the prompt
 
 # ADDING BONUS FEATURES #
 # 1. Better integer validation
@@ -19,6 +20,113 @@
 #   Because there are lots of messages to the program, we could move them into a
 #   configuration file and access by key.
 # 5. Internationalize the message in your calculator.
-  Using the configuration file,send message to call the right translation to cod
-  validate that only a language is choosen/ message that only certain language
-  exists
+  # Using the configuration file,send message to call the right translation to cod
+  # validate that only a language is choosen/ message that only certain language
+  # exists
+
+def prompt(message)
+  Kernel.puts("=> #{message}")
+end
+
+def valid_number?(number)
+  number.to_i != 0
+end
+
+def operation_to_message(op)
+  case op
+  when '1'
+    'Adding'
+  when '2'
+    'Subtracting'
+  when '3'
+    'Multiplying'
+  when '4'
+    'Dividing'
+  end
+end
+
+prompt("Welcome to the calculator! Enter your name:  ")
+
+name = ''
+loop do
+  name = Kernel.gets().chomp()
+
+  if name.empty?()
+    prompt("Make sure to use a valid name.")
+  else
+    break
+  end
+end
+
+prompt("Hi #{name}")
+loop do # main loop
+  # Get user's numbers
+  first_number = ''
+  loop do
+    prompt("Please input your first number")
+    first_number = Kernel.gets().chomp()
+
+    if valid_number?(first_number)
+      break
+    else
+      prompt("Hmmm... that doesnt look like a valid number")
+    end
+  end
+
+  second_number = ''
+  loop do
+    prompt("Please input your second number")
+    second_number = Kernel.gets().chomp()
+
+    if valid_number?(second_number)
+      break
+    else
+      prompt("Hmmm... that doesnt look like a valid number")
+    end
+  end
+
+  # Get user's choice of operation
+  operator_prompt = <<-MSG
+    "Which operation would you like to perform?
+    1) add
+    2) subtract
+    3) multiply
+    4) divide
+  MSG
+
+  prompt(operator_prompt)
+
+  operator = ''
+  loop do
+    operator = Kernel.gets().chomp()
+
+    if %w(1 2 3 4).include?(operator)
+      break
+    else
+      prompt("Must be 1, 2, 3, or 4")
+    end
+  end
+
+  prompt("#{operation_to_message(operator)} the two numbers....")
+
+  # refactor using case statement
+
+  results = case operator
+            when '1'
+              first_number.to_i() + second_number.to_i()
+            when '2'
+              first_number.to_i() - second_number.to_i()
+            when '3'
+              first_number.to_i() * second_number.to_i()
+            when '4'
+              first_number.to_f() / second_number.to_f()
+            end
+
+  prompt("The result is #{results}")
+
+  prompt("Do you want to perform another calculation? (Y to calculate again)")
+  answer = Kernel.gets().chomp()
+  break unless answer.downcase().start_with?('y')
+end
+
+prompt("Thank you for using the calculator. Goodbye!")
