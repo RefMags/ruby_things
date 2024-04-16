843f8cb3735407204b458544cddf37641239e069
refilwe magwaneng <fyfymags@gmail.com>
2024-04-17 05:50:38 +0900

    loops and iterators exercise- START

---
+++ RB100/_exercises/5_loops_and_iterators.rb
@@ -0,0 +1,11 @@
+# What does each method in the following program return after it is finished executing?
+
+x = [1, 2, 3, 4, 5]
+x.each do |a|
+  a + 1
+end
+
+# Write a while loop that takes inputs from user, perfoms an action and only stops when the user types "STOP". Each loop can get info from the user.
+
+
+# write a method that counts down to zero using recursion
