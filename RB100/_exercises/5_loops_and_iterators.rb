e40052997581c426e2de5e8fbb71a28ba63cff53
refilwe magwaneng <fyfymags@gmail.com>
2024-04-18 06:14:25 +0900

    loops and iterators completed

--- RB100/_exercises/5_loops_and_iterators.rb
+++ RB100/_exercises/5_loops_and_iterators.rb
@@ -5,7 +5,26 @@ x.each do |a|
   a + 1
 end
 
-# Write a while loop that takes inputs from user, perfoms an action and only stops when the user types "STOP". Each loop can get info from the user.
+# the array in which we invoked each returns the original array
 
+# Write a while loop that takes inputs from user, performs an action and only stops when the user types "STOP". Each loop can get info from the user.
+
+x = ' '
+
+while x != "STOP" do
+  puts "What is favorite animal?"
+  answer = gets.chomp
+  puts "Would you like the next question?"
+  x = gets.chomp
+end
 
 # write a method that counts down to zero using recursion
+
+def zero_counter(number)
+  if number <= 0
+    puts number
+  else
+    puts number
+    zero_counter(number - 1)
+  end
+end
---
+++ RB100/_exercises/questions.md
@@ -0,0 +1,1 @@
+1. in loops and iterators, what are some of the iterator methods that can be used?
