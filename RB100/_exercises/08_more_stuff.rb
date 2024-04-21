44bfc875cbdd221610e34acb016e71c84e621808
refilwe magwaneng <fyfymags@gmail.com>
2024-04-22 06:24:21 +0900

    more stuff exercise begin

---
+++ RB100/_exercises/08_more_stuff.rb
@@ -0,0 +1,20 @@
+#1. write a program that checks if the sequence of characters "lab" exist in the following strings.
+# if it exists print out the word
+
+# 2. what will the following program print?
+
+#3. what is exception handling and what problem does it solve?
+
+# 4. Modify the code in exercise 2 to make the block execute properly.
+
+# 5. Why does the following code
+
+# def execute(block)
+#   block.call
+# end
+
+# execute { puts "Hello from inside the execute method!" }
+
+# give us the following error when we run it?
+# block.rb1:in `execute': wrong number of arguments (0 for 1) (ArgumentError)
+# from test.rb:5:in `<main>'
