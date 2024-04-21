7f816cecb9a40f4484cdd447a2d176d0c541174c
refilwe magwaneng <fyfymags@gmail.com>
2024-04-21 17:16:30 +0900

    array completed

--- RB100/_exercises/6_arrays.rb
+++ RB100/_exercises/6_arrays.rb
@@ -2,30 +2,39 @@
 arr = [1, 3, 5, 7, 9, 11]
 number = 3
 
-arr.include?(number)
+arr.each do |num|
+  if num == number
+    puts "#{number} is in the array."
+  end
+end
+
+if arr.include?(number)
+  puts "Positive for #{number} present"
+end
 
 # 2. What will the following program return? What is the value of arr after each?
 
 arr = ["b", "a"]
-arr = arr.product(Array(1..3))
+arr = arr.product(Array(1..3)) # pass in Array(1..3) argument
 arr.first.delete(arr.first.last)
 
 
+
 arr = ["b", "a"]
-arr = arr.product([Array(1..3)])
+arr = arr.product([Array(1..3)]) # [Array(1..3)] wraps the return value of generating elements of array 1 to 3
 arr.first.delete(arr.first.last)
 
 # 3. How do you return the word "example" from the following array?
 
 arr = [["test", "hello", "world"], ["example", "mem"]]
 
-arr[1][0]
+puts arr[1][0]
 
 # 4. how does each method return in the following example?
 arr = [15, 7, 18, 5, 12, 8, 5, 1]
 
-arr.index(5) # index returns the index of a specified element therefor the index of the element 5 from the arr is 3.
-array.index[5] # returns a NoMethodError: undefined '[]' for Enumerator
+puts arr.index(5) # index returns the index of a specified element, therefor the index position of the element 5 from the arr is 3.
+# array.index[5] # returns a NoMethodError: undefined '[]' for Enumerator
 arr[5] # returns the element at the specified index therefore return is 8
 
 # 5. what is the value of a, b and c in the following program?
@@ -38,7 +47,7 @@ c = string[19] # returns 'nil'
 # 6. You run the following code...
 
 names = ['bob', 'joe', 'susan', 'margaret']
-names['margaret'] = 'jody'
+# names['margaret'] = 'jody'
 
 # ... and get the following error message:
 
@@ -58,8 +67,8 @@ names[3] = 'jody'
 
 schools = ['Livingstone', 'Materspei', 'Ledumang', 'Yokohama']
 
-schools.each_with_index do |index, value|
-  puts "#{value}. #{index}"
+schools.each_with_index do |value, index|
+  puts "#{index}. #{value}"
 end
 
 # 8. Write a program that iterates over an array and builds a new array that is the result of incrementing each value in the original array by value of 2
