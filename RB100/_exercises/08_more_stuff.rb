422518813b683697702aae08c4a8d61e8d08d163
refilwe magwaneng <fyfymags@gmail.com>
2024-04-24 06:28:12 +0900

    regex completed

--- RB100/_exercises/08_more_stuff.rb
+++ RB100/_exercises/08_more_stuff.rb
@@ -1,7 +1,25 @@
 #1. write a program that checks if the sequence of characters "lab" exist in the following strings.
 # if it exists print out the word
 
+def has_lab(string)
+  if string =~ /lab/
+    puts "#{string}"
+  else
+    puts "lab is not present in the #{string}"
+  end
+end
+has_lab("laboratory")
+has_lab("experiment")
+has_lab("Pans Labyrinth")
+has_lab("elaborate")
+has_lab("polar bear")
+
 # 2. what will the following program print?
+def execute(&block)
+  block.call
+end
+
+execute { puts "Hello from inside the execute method!" }
 
 #3. what is exception handling and what problem does it solve?
 
