14baae168acf49ba22d0c3aacf52dd73dbec13ff
refilwe magwaneng <fyfymags@gmail.com>
2024-04-26 05:44:13 +0900

    more stuff for intro complete

--- RB100/_exercises/08_more_stuff.rb
+++ RB100/_exercises/08_more_stuff.rb
@@ -15,15 +15,23 @@ has_lab("elaborate")
 has_lab("polar bear")
 
 # 2. what will the following program print?
-def execute(&block)
+def execute(block)
   block.call
 end
 
 execute { puts "Hello from inside the execute method!" }
 
 #3. what is exception handling and what problem does it solve?
+# a process of dealing with error in a manageable and predictable way.
+# When a user enters wrong/bad information
+# when a file-manipulating process gets corrupted.
 
 # 4. Modify the code in exercise 2 to make the block execute properly.
+def execute(&block)
+  block.call
+end
+
+execute { puts "Hello from inside the execute method!" }
 
 # 5. Why does the following code
 
@@ -36,3 +44,4 @@ execute { puts "Hello from inside the execute method!" }
 # give us the following error when we run it?
 # block.rb1:in `execute': wrong number of arguments (0 for 1) (ArgumentError)
 # from test.rb:5:in `<main>'
+# the method definition parameter is missing the amperand(&) which allows the block to be passed as argument
