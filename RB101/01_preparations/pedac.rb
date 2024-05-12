f9f83a8ef15d77824e0ea6d41a61d7cd548639e1
refilwe magwaneng <fyfymags@gmail.com>
2024-05-13 06:43:21 +0900

    PEDAC code from working with small problems

---
+++ RB101/01_preparations/pedac.rb
@@ -0,0 +1,20 @@
+# This is an example of PEDAC problem adopted from readings.
+
+def sum_of_multiples(target, factors)
+  multiples = []
+  factors = [3, 5] if factors.length == 0
+
+  factors.each do |factor| # we are calling `each` method on the variable `factors` and passing in the block as a parameter.
+    current_multiple = factor
+
+    while current_multiple < target
+      multiples << current_multiple
+      current_multiple += factor
+    end
+  end
+
+  multiples.uniq.inject(0, :+)
+end
+
+puts sum_of_multiples(20, [3, 5])
+puts sum_of_multiples(15, [3, 5])
