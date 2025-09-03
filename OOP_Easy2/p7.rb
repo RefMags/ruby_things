7f0e1ce46e146cb485796c88d36359c46d327b19
Ref Mags <fyfymags@gmail.com>
2025-09-03 20:36:33 +0900

    deconstructing programs, modules and collaborator objects

---
+++ OOP_Easy2/p7.rb
@@ -0,0 +1,55 @@
+class Pet
+  attr_reader :pet_type, :pet_name
+
+  def initialize(pet_type, pet_name)
+    @pet_type = pet_type
+    @pet_name = pet_name
+  end
+end
+
+class Owner
+
+end
+
+class Shelter
+
+end
+
+butterscotch = Pet.new('cat', 'Butterscotch')
+pudding      = Pet.new('cat', 'Pudding')
+darwin       = Pet.new('bearded dragon', 'Darwin')
+kennedy      = Pet.new('dog', 'Kennedy')
+sweetie      = Pet.new('parakeet', 'Sweetie Pie')
+molly        = Pet.new('dog', 'Molly')
+chester      = Pet.new('fish', 'Chester')
+
+phanson = Owner.new('P Hanson')
+bholmes = Owner.new('B Holmes')
+
+shelter = Shelter.new
+shelter.adopt(phanson, butterscotch)
+shelter.adopt(phanson, pudding)
+shelter.adopt(phanson, darwin)
+shelter.adopt(bholmes, kennedy)
+shelter.adopt(bholmes, sweetie)
+shelter.adopt(bholmes, molly)
+shelter.adopt(bholmes, chester)
+shelter.print_adoptions
+puts "#{phanson.name} has #{phanson.number_of_pets} adopted pets."
+puts "#{bholmes.name} has #{bholmes.number_of_pets} adopted pets."
+
+=begin
+P Hanson has adopted the following pets:
+a cat named Butterscotch
+a cat named Pudding
+a bearded dragon named Darwin
+
+B Holmes has adopted the following pets:
+a dog named Molly
+a parakeet named Sweetie Pie
+a dog named Kennedy
+a fish named Chester
+
+P Hanson has 3 adopted pets.
+B Holmes has 4 adopted pets.
+=end
--- OOP_Easy2/p9.rb
+++ OOP_Easy2/p9.rb
@@ -1,10 +1,13 @@
 module Walkable
   def walk
-   puts " struts forward"
+  #  puts "#{title} #{name} struts forward"
+  "struts forward"
   end
 end
 
 class Noble
+  include Walkable
+
   attr_reader :name, :title
 
   def initialize(name, title)
@@ -12,7 +15,10 @@ class Noble
     @title = title
   end
 
-  include Walkable
+  # We override the walk method from the Walkable method, interpolate state, and use super to call the method's version
+  def walk
+    puts "#{title} #{name} #{super}"
+  end
 end
 
 byron = Noble.new("Byron", "Lord")
